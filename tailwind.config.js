94eade8e7d6a2dfeea48c371894ce0e1fd19ec31
00lar <eduardani84@gmail.com>
2023-02-07 23:53:11 -0700

    Primera seccion del nav y Font Family asignada

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./src/**/*.{html,js}", "./index.html"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        Montserrad: ["Montserrat", "sans-serif"],
+      },
+    },
   },
   plugins: [],
-}
+};
